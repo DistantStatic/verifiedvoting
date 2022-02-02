e3034c4e92a79298cb8d1d69f2ec45a10b03f988
Oscar Rodriguez <oscarrodriguez101@gmail.com>
2022-02-01 21:48:01 -0500

    wired modals to app

--- src/App.js
+++ src/App.js
@@ -5,13 +5,30 @@ import Navigation from './components/navigation/navigation';
 import MainPage from './containers/main-page/main-page';
 import Web3Container from './context-providers/web3-provider/web3-provider';
 import ContractContainer from './context-providers/contract-provider/contract-provider';
+import EnterRaceModal from './modals/enter-race/enter-race';
+import VoteModal from './modals/vote/vote';
+import { useState } from 'react';
 
 function App() {
+    
+    const [voteModal, setVoteModal] = useState(false);
+    const [raceModal, setRaceModal] = useState(false);
+
+    const showVoteModal = () => {
+        setVoteModal(true);
+    }
+
+    const showRaceModal = () => {
+        setRaceModal(true);
+    }
+
     return (
         <Web3Container>
             <ContractContainer>
-                <Navigation />
+                <Navigation raceShow={showRaceModal} voteShow={showVoteModal} />
                 <MainPage />
+                <EnterRaceModal show={raceModal} hide={() => setRaceModal(false)}/>
+                <VoteModal show={voteModal} hide={() => setVoteModal(false)} />
             </ContractContainer>
         </Web3Container>
     );
